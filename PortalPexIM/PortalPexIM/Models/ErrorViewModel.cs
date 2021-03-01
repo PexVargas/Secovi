ac4570274133d195d3176a9aab968dda1fc7ce43
Vinicius Brasil <vinicius.brasil@pexsoft.com.br>
2021-03-01 17:39:51 -0300

    Start
    Start

---
+++ PortalPexIM/PortalPexIM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PortalPexIM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
