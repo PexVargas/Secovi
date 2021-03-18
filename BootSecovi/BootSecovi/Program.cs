08220ad579200ed72a852a199c0e3e35a2302127
Vinicius Brasil <vinicius.brasil@pexsoft.com.br>
2021-03-18 13:57:11 -0300

    Zap

--- BootSecovi/BootSecovi/Program.cs
+++ BootSecovi/BootSecovi/Program.cs
@@ -7,17 +7,12 @@ namespace BootSecovi
     {
         static void Main(string[] args)
         {
-            // Zap zap = new Zap();
-            // zap.Coletar("RJ", "Rio de Janeiro", 2);
+            Zap zap = new Zap();
+            zap.Coletar("RJ", "Rio de Janeiro", 2);
 
-            //VivaReal vivaReal = new VivaReal();
-            //vivaReal.Coletar("RJ", "Rio de Janeiro", 2);
 
-            //Classificador c = new Classificador();
-
-            //c.Classificar("RJ");
-
-            GeradorCSV.GeradorZapRJ();
+            Console.WriteLine("Processo finalizado com sucesso!");
+            Console.ReadKey();
         }
     }
 }
