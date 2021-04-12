a3f4b060f5d29de88a682beeecd63b49c40ff8cc
Vinicius <Viniciusbss124@gmail.com>
2021-04-12 02:50:24 -0300

    with skip

--- ImobiliariasCrawler/ImobiliariasCrawler.Main/Spiders/Prod/SpiderBase.cs
+++ ImobiliariasCrawler/ImobiliariasCrawler.Main/Spiders/Prod/SpiderBase.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using ImobiliariasCrawler.Main.DataObjectTransfer;
 using ImobiliariasCrawler.Main.Model;
 using ImobiliariasCrawler.Main.Services;
 using System;
@@ -16,39 +17,60 @@ namespace ImobiliariasCrawler.Main.Spiders
 
     public abstract class SpiderBase : IDisposable
     {
-        public List<ImoveiscapturadosDto> Items { get; set; }
+        public List<Imoveiscapturados> Items { get; set; }
         public RequestService Request { get; set; }
         private readonly PexinContext _context;
+        private LoggingPerMinuteDto _logging;
+        private readonly int _buffer;
+        private int countTotalInsert = 0;
+        private int lastSkip = 0;
 
-        public SpiderBase()
+        public SpiderBase(int buffer = 10, int concurrentRequests = 10)
         {
-            Items = new List<ImoveiscapturadosDto>();
-            Request = new RequestService(new HttpClient());
+            _buffer = buffer;
+            _logging = new LoggingPerMinuteDto() { Spider = GetType().Name };
+            Items = new List<Imoveiscapturados>();
+            Request = new RequestService(new HttpClient(), _logging, Close);
             _context = new PexinContext();
         }
         public void Init() {
             StartRequest();
         }
+    
 
         public abstract void StartRequest();
         public abstract void Parse(Response response);
-
         public void Save(ImoveiscapturadosDto imoveiscapturados)
         {
+            _logging.CountItems++;
+            Items.Add(imoveiscapturados.ToImoveiscapturados());
+            InsertItems();
+        }
+
+        private void InsertItems(bool close = false)
+        {
+            if (Items.Count <= _buffer && !close) return;
             lock (_context)
             {
-                Console.SetCursorPosition(0, 15);
-                Console.WriteLine(imoveiscapturados.Url);
-                _context.Imoveiscapturados.AddRange(Items.Select(i => i.ToImoveiscapturados()));
+                var newList = Items.Skip(lastSkip).Take(_buffer).ToList();
+                countTotalInsert += newList.Count;
+                lastSkip += _buffer;
+                _context.AddRange(newList);
                 _context.SaveChanges();
             }
+        }
+
+        public virtual void Close()
+        {
+            InsertItems();
+            Dispose();
+            Console.WriteLine($"FINISH SPIDER [{_logging.Spider}] - Inseridos [{countTotalInsert}] - Requests [{_logging.CounRequests}] - Items [{_logging.CountItems}]");
         }
 
         public void Dispose()
         {
             _context.Dispose();
             Request.Dispose();
-            Console.WriteLine("Salvado");
         }
     }
 }
