50490a41f26895faa581be1d56f7ff8828eb6baf
Vinicius <Viniciusbss124@gmail.com>
2021-04-13 17:40:53 -0300

    retirando código desnecessário

--- ImobiliariasCrawler/ImobiliariasCrawler.Main/Extensions/ObjectExtensions.cs
+++ ImobiliariasCrawler/ImobiliariasCrawler.Main/Extensions/ObjectExtensions.cs
@@ -42,49 +42,5 @@ namespace ImobiliariasCrawler.Main.Extensions
         {
             throw new NullReferenceException("Unable to convert anonymous object to a dictionary. The source anonymous object is null.");
         }
-
-        //public static IDictionary<string, string> ToKeyValue(this object metaToken)
-        //{
-        //    if (metaToken == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    var serializer = new JsonSerializer { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
-        //    var token = metaToken as JToken;
-        //    if (token == null)
-        //    {
-        //        return ToKeyValue(JObject.FromObject(metaToken, serializer));
-        //    }
-
-        //    if (token.HasValues)
-        //    {
-        //        var contentData = new Dictionary<string, string>();
-        //        foreach (var child in token.Children().ToList())
-        //        {
-        //            var childContent = child.ToKeyValue();
-        //            if (childContent != null)
-        //            {
-        //                contentData = contentData.Concat(childContent)
-        //                                         .ToDictionary(k => k.Key.ToLower(), v => v.Value);
-        //            }
-        //        }
-
-        //        return contentData;
-        //    }
-
-        //    var jValue = token as JValue;
-        //    if (jValue?.Value == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    var value = jValue?.Type == JTokenType.Date ?
-        //                    jValue?.ToString("o", CultureInfo.InvariantCulture) :
-        //                    jValue?.ToString(CultureInfo.InvariantCulture);
-
-        //    return new Dictionary<string, string> { { token.Path, value } };
-        //}
-
     }
 }
