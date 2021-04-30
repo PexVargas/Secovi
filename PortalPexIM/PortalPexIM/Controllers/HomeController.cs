ce5cda6f02ec13be2b6a4f967fdc3460213a54c2
eduardo_vargas <eduardo.vargas@plugar.com.br>
2021-04-30 14:09:41 -0300

    ajuste valor medio

--- PortalPexIM/PortalPexIM/Controllers/HomeController.cs
+++ PortalPexIM/PortalPexIM/Controllers/HomeController.cs
@@ -78,6 +78,36 @@ namespace PortalPexIM.Controllers
             return Json(imoveis);
         }
 
+        //[HttpPost]
+        //public JsonResult GetTipos([FromBody] FiltroPesquisa filtro)
+        //{
+        //    peximContext dbt = new peximContext();
+        //    var siglaEstado = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        //    var mes = Convert.ToInt32(filtro.DtReferencia.Split("/")[0]);
+        //    var ano = Convert.ToInt32(filtro.DtReferencia.Split("/")[1]);
+        //    var dataBase = new DateTime(ano, mes, 1);
+
+        //    var imoveis = (from i in dbt.Imoveisclassificados
+        //                   where i.Tipo != null
+        //                    && ((filtro.Cidades == null || filtro.Cidades.Length == 0) || filtro.Cidades.Contains(i.Cidade))
+        //                    && ((filtro.Bairros == null || filtro.Bairros.Length == 0) || filtro.Bairros.Contains(i.Bairro))
+        //                    && ((filtro.Tipos == null || filtro.Tipos.Length == 0) || filtro.Tipos.Contains(i.Tipo))
+        //                    && i.TipoImovel == filtro.TipoImovel
+        //                    && i.Excluido == 0
+        //                    && i.SiglaEstado == siglaEstado
+        //                    && i.DataClassificacao == (dataBase)
+        //                   group i by new { i.Tipo } into g
+
+        //                   select new
+        //                   {
+        //                       key = g.Key.Tipo,
+        //                       value = g.Count(),
+        //                   }).ToList().OrderByDescending(x=>x.value);
+
+        //    return Json(imoveis);
+        //}
+
+
         [HttpPost]
         public JsonResult GetTipos([FromBody] FiltroPesquisa filtro)
         {
@@ -87,7 +117,7 @@ namespace PortalPexIM.Controllers
             var ano = Convert.ToInt32(filtro.DtReferencia.Split("/")[1]);
             var dataBase = new DateTime(ano, mes, 1);
 
-            var imoveis = (from i in dbt.Imoveisclassificados
+            var lstImoveis = (from i in dbt.Imoveisclassificados
                            where i.Tipo != null
                             && ((filtro.Cidades == null || filtro.Cidades.Length == 0) || filtro.Cidades.Contains(i.Cidade))
                             && ((filtro.Bairros == null || filtro.Bairros.Length == 0) || filtro.Bairros.Contains(i.Bairro))
@@ -96,16 +126,89 @@ namespace PortalPexIM.Controllers
                             && i.Excluido == 0
                             && i.SiglaEstado == siglaEstado
                             && i.DataClassificacao == (dataBase)
-                           group i by new { i.Tipo } into g
+                           select new Imovel
+                           {
+                               Tipo = i.Tipo,
+                               Valor = (i.Valor == null ? 0 : i.Valor),
+                               ValorTotal = (i.Valor == null ? 0 : i.Valor),
+                               AreaPrivativa = i.AreaPrivativa,
+                               AreaTotal = i.AreaTotal,
+                           }).OrderBy(x => x.Valor).ToList();
 
+
+            var Tipos = BuscarTipos(lstImoveis, "valorMedioMetros", 1 );
+
+            var imoveis = (from i in Tipos
                            select new
                            {
-                               key = g.Key.Tipo,
-                               value = g.Count(),
-                           }).ToList().OrderByDescending(x=>x.value);
+                               key = i.Chave,
+                               value = i.Valor,
+                           }).ToList().OrderByDescending(x => x.value);
 
             return Json(imoveis);
         }
+
+        public List<BaseDados> BuscarTipos(List<Imovel> imoveis,  string unidade, int? tipoArea)
+        {
+            if (unidade != "valorMedioMetros")
+                tipoArea = 2;
+
+            List<BaseDados> tiposResult = new List<BaseDados>();
+
+            var dadosAgrupados = (from i in imoveis
+                                  select new
+                                  {
+                                      Chave = i.Tipo,
+                                      Valor = i.Valor,
+                                      Area = (tipoArea == 1 ? i.AreaTotal : i.AreaPrivativa),
+                                  }).GroupBy(x => x.Chave).ToList();
+
+            foreach (var dados in dadosAgrupados)
+            {
+                if (dados.Key != null)
+                {
+                    BaseDados baseDados = new BaseDados();
+
+                    var valores = (from i in imoveis
+                                   where i.Tipo == dados.Key
+                                
+                                   select new ImovelOutlier()
+                                   {
+                                       valor = Convert.ToDouble(i.Valor),
+                                       Area = (tipoArea == 1 ? i.AreaTotal : i.AreaPrivativa),
+                                   }).ToList();
+
+
+                    var dadosGrupo = dados.FirstOrDefault();
+                    baseDados.Chave = dadosGrupo.Chave;
+
+                    if (unidade == "quantidade")
+                    {
+                        var valor = valores.Count;
+                        baseDados.Valor = valor;
+                        baseDados.Quantidade = valor;
+                        baseDados.Metragem = valores.Where(x => x.Area > 0).Average(x => x.Area);
+                    }
+                    else
+                    {
+                        var grupo = AplicarOutLier(valores);
+                        baseDados.Valor = Convert.ToDecimal(grupo.Valor);
+                        baseDados.Quantidade = grupo.Quantidade;
+                        baseDados.Minimo = grupo.Minimo;
+                        baseDados.Maximo = grupo.Maximo;
+                        baseDados.Metragem = grupo.Metragem;
+                    }
+
+                    tiposResult.Add(baseDados);
+                }
+            }
+
+            if (unidade == "quantidade")
+                return tiposResult.OrderByDescending(x => x.Quantidade).Where(x => x.Quantidade > 0).ToList();
+            else
+                return tiposResult.OrderByDescending(x => x.Valor).Where(x => x.Quantidade > 0).ToList();
+        }
+
 
 
         [HttpPost]
@@ -192,6 +295,139 @@ namespace PortalPexIM.Controllers
                            }).ToList();
 
             return Json(imoveis);
+        }
+
+
+        public BaseDados AplicarOutLier(List<ImovelOutlier> imoveis)
+        {
+            var outlierGeral = false;
+
+            BaseDados baseDados = new BaseDados();
+
+            if (imoveis.Count > 9)
+            {
+                imoveis = imoveis.OrderBy(x => x.valor).ToList();
+                var meioListagem = Convert.ToInt32((imoveis.Count / 2));
+
+                for (int i = meioListagem; i >= 0; i--)
+                {
+                    if (imoveis[i].valor > 0)
+                    {
+                        double imovelSuperior = imoveis[i + 1].valor;//E4
+                        var imovelInferior = imoveis[i].valor;//E3 aqui é o ultimo
+
+                        if (imovelSuperior <= (imovelInferior + (imovelInferior * 0.50))) //verdadeiro
+                            imoveis[i].outlier = outlierGeral;
+                        else
+                        {
+                            outlierGeral = true;
+                            imoveis[i].outlier = outlierGeral;
+                        }
+                    }
+                    else
+                        imoveis[i].outlier = true;
+                }
+
+                outlierGeral = false;
+
+                ////varre do  meio  para o final
+                for (int i = meioListagem + 1; i < imoveis.Count; i++)
+                {
+                    if (imoveis[i].valor > 0)
+                    {
+                        var imovelSuperior = imoveis[i].valor;//E4
+                        var imovelAnterior = imoveis[i - 1].valor;//E3
+
+                        imovelAnterior += (imovelAnterior * 0.50);
+
+                        if (imovelAnterior >= imovelSuperior)
+                            imoveis[i].outlier = outlierGeral;
+                        else
+                        {
+                            outlierGeral = true;
+                            imoveis[i].outlier = outlierGeral;
+                        }
+                    }
+                    else
+                        imoveis[i].outlier = true;
+                }
+
+                var contador = 0;
+                double valorTotalPrimeiroCorte = 0;
+
+                for (var i = 0; i < imoveis.Count; i++)
+                {
+                    if (imoveis[i].outlier == false)
+                    {
+                        valorTotalPrimeiroCorte += imoveis[i].valor;
+                        contador++;
+                    }
+                }
+
+                var MediaPrimerioCorte = (valorTotalPrimeiroCorte / contador);
+                MediaPrimerioCorte = Math.Round(MediaPrimerioCorte, 4);
+                var ValorCorteInicial = (MediaPrimerioCorte - (MediaPrimerioCorte * 0.40));
+                var ValorCorteFinal = (MediaPrimerioCorte + (MediaPrimerioCorte * 0.40));
+
+                //somente os que não são outliers
+                for (int i = 0; i < imoveis.Count; i++)
+                {
+                    if (imoveis[i].outlier == false)
+                    {
+                        if (imoveis[i].valor < ValorCorteInicial)
+                            imoveis[i].outlier = true;
+                        else if (imoveis[i].valor > ValorCorteFinal)
+                            imoveis[i].outlier = true;
+                    }
+                }
+
+                var contadorSegundoCorte = 0;
+                double valorTotalSegundoCorte = 0;
+
+                for (var i = 0; i < imoveis.Count; i++)
+                {
+                    if (imoveis[i].outlier == false)
+                    {
+                        valorTotalSegundoCorte += imoveis[i].valor;
+                        contadorSegundoCorte++;
+                    }
+                }
+
+                var MediaValor = 0;
+
+                if (valorTotalSegundoCorte == 0 && contadorSegundoCorte == 0) { }
+                else
+                {
+                    baseDados.lstImovelOutlier = imoveis.Where(x => x.outlier == false).ToList();
+                    baseDados.Quantidade = contadorSegundoCorte;
+                    valorTotalSegundoCorte = Math.Round(valorTotalSegundoCorte, 4);
+
+                    decimal valor = Convert.ToDecimal(valorTotalSegundoCorte / contadorSegundoCorte);
+
+                    baseDados.Valor = Math.Round(valor, 2, MidpointRounding.AwayFromZero);
+
+                    baseDados.Minimo = imoveis.Where(x => x.outlier == false).Min(x => x.valor);
+                    baseDados.Maximo = imoveis.Where(x => x.outlier == false).Max(x => x.valor);
+                    baseDados.Metragem = Convert.ToDecimal(imoveis.Where(x => x.outlier == false && x.Area > 0).Average(x => x.Area));
+                    return baseDados; 
+                }
+       
+            }
+            else
+            {
+                if (imoveis.Count > 0)
+                {
+                    baseDados.Valor = Convert.ToDecimal(imoveis.Average(x => x.valor));
+                    baseDados.Quantidade = imoveis.Count();
+
+                    baseDados.Minimo = imoveis.Min(x => x.valor);
+                    baseDados.Maximo = imoveis.Max(x => x.valor);
+                    baseDados.Metragem = Convert.ToDecimal(imoveis.Where(x => x.Area > 0).Average(x => x.Area));
+                }
+                return baseDados;
+            }
+
+            return baseDados;//verificar melhor retorno
         }
 
         static string FormatarGaragem(int? garagens)
